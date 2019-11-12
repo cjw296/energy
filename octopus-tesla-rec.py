b87f8fe7a0a34fdbd35db9a82636defeda76015a
Chris Withers <chris@withers.org>
2019-11-11 22:52:15 -0500

    Handle days with DST changes.
    Weird that we don't see the same problem with the Octopus data!

--- octopus-tesla-rec.py
+++ octopus-tesla-rec.py
@@ -29,7 +29,9 @@ def find_dates(storage: Path):
 
 def reconcile(storage: Path, date: Date, threshold: float):
     tesla = pd.read_csv(storage / f'tesla-{date}.csv',
-                        index_col='Date time', parse_dates=['Date time'])
+                        index_col='Date time',
+                        parse_dates=['Date time'],
+                        date_parser=lambda col: pd.to_datetime(col, utc=True))
     # blank out any energy sent back to the grid, octopus is consumption only:
     tesla[tesla['Grid (kW)']<0] = 0
     # Tesla provide power flow every 5 mins, let's assume that represents the continous
