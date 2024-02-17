b8fd33ea489bc035065e680b6f64a13c8aeb272d
Chris Withers <chris@simplistix.co.uk>
2024-02-17 12:46:20 +0000

    Cleanup and add date inside json dump

--- octopus-tesla-sync.py
+++ octopus-tesla-sync.py
@@ -1,16 +1,15 @@
 import logging
 from argparse import ArgumentParser
-from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
-from pprint import pprint, pformat
+from pprint import pformat
 
 from configurator import Config
-from gql import gql
+from gql.transport.aiohttp import log as gql_logger
 
 from common import DiffDumper, add_log_level, configure_logging, Run
-from octopus import OctopusRESTClient, OctopusGraphQLClient
+from octopus import OctopusGraphQLClient
 
-from gql.transport.aiohttp import log as gql_logger
 gql_logger.setLevel(logging.WARNING)
 
 
@@ -22,7 +21,9 @@ def record_octopus_dispatches(
     dispatches = graphql_client.dispatches(account)
     unit_rates = graphql_client.unit_rates(account)
     logging.info(pformat(dispatches))
-    dumper.update({'dispatches': dispatches, 'unit_rates': unit_rates})
+    dumper.update(
+        {'now': datetime.now().isoformat(), 'dispatches': dispatches, 'unit_rates': unit_rates}
+    )
 
 
 def main():
@@ -37,7 +38,6 @@ def main():
     api_key = config.octopus.api_key
     account = config.octopus.account
 
-    rest_client = OctopusRESTClient(api_key)
     graphql_client = OctopusGraphQLClient(api_key)
     dumper = DiffDumper(Path(config.directories.storage).expanduser(), prefix='octopus-dispatches')
 
