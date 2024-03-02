21e854ca2493ad835930d8a9070dcccfc4b81eed
Chris Withers <chris@simplistix.co.uk>
2024-03-02 10:42:50 +0000

    Allow filtering to a time range when showing changes

--- show-changes.py
+++ show-changes.py
@@ -7,8 +7,9 @@ from pathlib import Path
 
 from colorama import Fore
 from configurator import Config
+from pandas import Timestamp
 
-from common import add_log_level, configure_logging
+from common import add_log_level, configure_logging, root_from
 
 
 def color_diff(diff):
@@ -38,9 +39,13 @@ PATTERN = "%Y-%m-%d-%H-%M-%S.json"
 DASH_COUNT = PATTERN.count('-')+1
 
 
-def extract(path: Path) -> DiffData:
+def datetime_from_path(path: Path) -> datetime:
     parts = path.name.rsplit('-', DASH_COUNT)
-    d = datetime.strptime('-'.join(parts[1:]), "%Y-%m-%d-%H-%M-%S.json")
+    return datetime.strptime('-'.join(parts[1:]), "%Y-%m-%d-%H-%M-%S.json")
+
+
+def extract(path: Path) -> DiffData:
+    d = datetime_from_path(path)
     return DiffData(
         f'{shlex.quote(str(path))} ({d:%a %d %b %y %H:%M:%S})',
         path.read_text().splitlines(keepends=True)
@@ -48,21 +53,27 @@ def extract(path: Path) -> DiffData:
 
 
 def main():
+    config = Config.from_path('config.yaml')
+    root = root_from(config)
+
     parser = ArgumentParser()
     parser.add_argument('prefix')
-    parser.add_argument('query', nargs='?')
+    parser.add_argument('--start', type=Timestamp)
+    parser.add_argument('--end', type=Timestamp)
     add_log_level(parser)
 
     args = parser.parse_args()
     configure_logging(args.log_level)
 
-    config = Config.from_path('config.yaml')
-    paths = sorted(Path(config.directories.storage).expanduser().glob(f'{args.prefix}*'))
+    paths = sorted(root.glob(f'{args.prefix}*'))
     for path1, path2 in zip(paths, paths[1:]):
-        print(diff(extract(path1), extract(path2)))
-        print()
+        if all((
+            args.start is None or datetime_from_path(path1) > args.start,
+            args.end is None or datetime_from_path(path2) < args.end,
+        )):
+            print(diff(extract(path1), extract(path2)))
+            print()
 
-    print(paths[-2:])
 
 if __name__ == '__main__':
     main()
