de761255b2380482dfc173196c95a0ecc362ab15
Chris Withers <chris@simplistix.co.uk>
2024-03-11 08:23:50 +0000

    Fill in up to 4 hours of missing standard unit rates
    Use the existing rate from the previous day.

--- schedule.py
+++ schedule.py
@@ -1,13 +1,13 @@
-from collections import defaultdict
+from datetime import UTC
 from zoneinfo import ZoneInfo
 
-from pandas import Timestamp
+from pandas import Timestamp, Timedelta
 
 from octopus import Schedule
 
-
 CHEAP_KEY = "SUPER_OFF_PEAK"
 EXPENSIVE_KEY = "ON_PEAK"
+MAX_ALLOWABLE_MISSING_STANDARD_UNIT_RATES = Timedelta(hours=4)
 
 
 def price_in_pounds(price_in_pence: float) -> float:
@@ -27,17 +27,29 @@ def make_energy_charges(cheap: float, expensive: float):
     }
 
 
+def unit_rates_sort_key(rate: dict[str, str | float]) -> Timestamp:
+    return Timestamp(rate['validFrom']).astimezone(UTC)
+
+
 def make_seasons_and_energy_charges(
         now: Timestamp, unit_rates_schedule: list[dict], dispatches: dict, timezone: ZoneInfo
 ) -> dict:
 
     unit_rates = set()
     schedule = Schedule(now)
-    for rate in unit_rates_schedule:
+
+    # add the standard unit rates to the schedule:
+    max_valid_to = None
+    value = None
+    for rate in sorted(unit_rates_schedule, key=unit_rates_sort_key):
+        valid_to = Timestamp(rate['validTo'])
+        max_valid_to = valid_to if max_valid_to is None else max(max_valid_to, valid_to)
         value = rate['value']
         unit_rates.add(value)
-        schedule.add(Timestamp(rate['validFrom']), Timestamp(rate['validTo']), value)
+        schedule.add(Timestamp(rate['validFrom']), valid_to, value)
+    assert max_valid_to is not None, 'empty unit rates?'
 
+    # figure out what the cheap and expensive rates are:
     assert len(unit_rates) == 2, f'Unexpected number of rates: {unit_rates}'
     cheap, expensive = sorted(unit_rates)
     labels = {
@@ -45,10 +57,25 @@ def make_seasons_and_energy_charges(
         expensive: EXPENSIVE_KEY
     }
 
+    # fill in any future, expected gaps in the standard unit rate schedule:
+    if max_valid_to < schedule.end:
+        missing = schedule.end - max_valid_to
+        if missing > MAX_ALLOWABLE_MISSING_STANDARD_UNIT_RATES:
+            hours = missing.total_seconds() / (60*60)
+            raise ValueError(
+                f'Missing standard unit rates for {hours:.1f} hours from {max_valid_to}'
+            )
+        fill_value = expensive if value == cheap else expensive
+        for start, existing_value in schedule.entries.items():
+            if existing_value is None and start >= max_valid_to:
+                schedule.entries[start] = fill_value
+
+    # Add any upcoming planned dispatches that are definitely marked as "smart charge"
     for dispatch in dispatches.get('plannedDispatches', ()):
         if dispatch['meta']['source'] == 'smart-charge':
             schedule.add(Timestamp(dispatch["startDtUtc"]), Timestamp(dispatch["endDtUtc"]), cheap)
 
+    # Build the final Tesla-compatible schedule
     summer_tou_periods = {
         EXPENSIVE_KEY: [],
         CHEAP_KEY: [],
--- test_make_seasons.py
+++ test_make_seasons.py
@@ -4,7 +4,7 @@ from zoneinfo import ZoneInfo
 from pandas import Timestamp
 
 from schedule import make_seasons_and_energy_charges
-from test_octopus import compare
+from testfixtures import compare, ShouldRaise
 
 London = ZoneInfo('Europe/London')
 
@@ -228,3 +228,60 @@ def test_planned_dispatch_is_not_smart_charge():
     )
     compare(json.loads(json.dumps(actual)), expected=BASIC_SCHEDULE)
 
+
+SHORT_UNIT_RATES_RESPONSE = [
+        {
+            "value": 7.49994,
+            "validFrom": "2024-03-04T23:30:00+00:00",
+            "validTo": "2024-03-05T05:30:00+00:00",
+        },
+        {
+            "value": 30.59805,
+            "validFrom": "2024-03-05T05:30:00+00:00",
+            "validTo": "2024-03-05T23:30:00+00:00",
+        },
+        {
+            "value": 7.49994,
+            "validFrom": "2024-03-05T23:30:00+00:00",
+            "validTo": "2024-03-06T05:30:00+00:00",
+        }
+    ]
+
+
+def test_unit_rates_fall_a_bit_short():
+    actual = make_seasons_and_energy_charges(
+        now=Timestamp('2024-03-05T07:05:30', tz=London),
+        unit_rates_schedule=SHORT_UNIT_RATES_RESPONSE,
+        dispatches={
+            "plannedDispatches": [],
+            "completedDispatches": []
+        },
+        timezone=London
+    )
+    compare(json.loads(json.dumps(actual)), expected=BASIC_SCHEDULE)
+
+
+def test_unit_rates_fall_a_lot__short():
+    actual = make_seasons_and_energy_charges(
+        now=Timestamp('2024-03-05T09:54:41', tz=London),
+        unit_rates_schedule=SHORT_UNIT_RATES_RESPONSE,
+        dispatches={
+            "plannedDispatches": [],
+            "completedDispatches": []
+        },
+        timezone=London
+    )
+    compare(json.loads(json.dumps(actual)), expected=BASIC_SCHEDULE)
+
+
+def test_unit_rates_fall_too_much__short():
+    with ShouldRaise(ValueError('Missing standard unit rates for 4.5 hours from 2024-03-06 05:30:00+00:00')):
+        make_seasons_and_energy_charges(
+            now=Timestamp('2024-03-05T10:15:30', tz=London),
+            unit_rates_schedule=SHORT_UNIT_RATES_RESPONSE,
+            dispatches={
+                "plannedDispatches": [],
+                "completedDispatches": []
+            },
+            timezone=London
+        )
